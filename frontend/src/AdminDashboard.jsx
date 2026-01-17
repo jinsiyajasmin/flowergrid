65c30ec8e070dc92bf40aa49a9c6cc1f5dff1c94
jinsiyajasmin <jinsiyajasmin9@gmail.com>
2026-01-17 15:12:52 +0530

    Update API URL to production

--- frontend/src/AdminDashboard.jsx
+++ frontend/src/AdminDashboard.jsx
@@ -33,7 +33,7 @@ import PeopleIcon from "../assets/none.png";
 
 
 
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://flowergrid.vercel.app";
 
 export default function AdminDashboard() {
   const [summaries, setSummaries] = useState([]);
