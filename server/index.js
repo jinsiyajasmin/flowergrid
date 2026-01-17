ba36a1e95ff251a1a480876a748704212189bf80
jinsiyajasmin <jinsiyajasmin9@gmail.com>
2026-01-17 17:16:00 +0530

    Fix Vercel 404: routing to /api and debug logs

--- server/index.js
+++ server/index.js
@@ -50,6 +50,13 @@ app.use(cors({
 }));
 
 app.get('/health', (req, res) => res.json({ status: "alive" }));
+app.get('/', (req, res) => res.send("FlowerGrid API is running"));
+
+// Log all requests for debugging
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use(express.json());
 
